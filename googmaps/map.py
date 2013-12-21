239ddde7ec48138bf4189c6136e4849c6d166f95
Sebastian Wilzbach <sebi@wilzbach.me>
2013-12-21 07:37:52 +0100

    Multiple Routes now possible

--- googmaps/map.py
+++ googmaps/map.py
@@ -7,6 +7,10 @@ from optparse import OptionParser
 from termcolor import colored, cprint
 from time import mktime
 import parsedatetime.parsedatetime as pdt
+import urllib2
+import time
+
+cal = pdt.Calendar()
 
 def main():
   args = sys.argv
@@ -16,67 +20,118 @@ def main():
   parser = OptionParser(usage=usage)
   parser.add_option("-m", "--mode", action="store", dest="mode", help="specifies type of transportation desired", default="driving")
   parser.add_option("-u", "--units", action="store", dest="units", help="specifies choice between metric and imperial systems")
+  parser.add_option("-z", "--no--url", action="store_true", dest="nourl", default=False, help="Disables URL-String")
   parser.add_option("-s", "--sensor", action="store", dest="sensor", default="false")
   parser.add_option("-a", "--arrival", action="store", dest="arrival_time", help="specifies desired time of arrival. can be stated in natural language")
   parser.add_option("-d", "--departure", action="store", dest="departure_time", help="specifies desired time of departure. can be stated in natural language")
   parser.add_option("-e", "--evade", action="store", dest="avoid", help="specifies choice in avoiding tolls or highways")
+  parser.add_option("-i", "--iterator", action="store", dest="iterator", help="how many times should the query be iterated?")
+ # parser.add_option("-t", "--time-sleep", action="store_true", dest="timesleep",default=False, help="Sleeps between every requests")
 
   (options, args) = parser.parse_args(args)
   if len(args) != 3:
     parser.error("Incorrect number")
-  make_url(parser, options, args)
 
+  ites =0
+  try:
+      ites = int(options.iterator)
+  except ( ValueError,AttributeError, TypeError) as e:
+      ites = 0
+  except Exception, e:
+      raise e
 
-def make_url(parser, options, args):
+  # check whether to use iterator
+  if ites == 0:
+     make_url(parser, options, args)
+  else:
+     for i in range(ites):
+        newOffset = make_url(parser, options, args, False)
+
+        options =increment_time(options, newOffset)
+
+def make_url(parser, options, args, printInfo=True):
   checkinput(options)
 
   url_end = ''
-  cal = pdt.Calendar()
 
   for key,value in options.__dict__.items():
     if(value != None):
       if key in ["departure_time", "arrival_time"]:
-        value = str(int(mktime(cal.parse(value)[0])))
-      re.sub(' ', '+', value)
-      url_end += key + '=' + value + '&'
-  
-  origin = re.sub(' ', '+', args[1]) 
+          try:
+            value = int(value)
+          except ValueError as e:
+            value = int(mktime(cal.parse(value)[0]))
+          finally:
+              time = value
+              value = str(value)
+      if not isinstance(value, bool):
+        re.sub(' ', '+', value)
+        url_end += key + '=' + value + '&'
+
+  origin = re.sub(' ', '+', args[1])
   destination = re.sub(' ', '+', args[2])
 
-  cprint ("To view these directions online, follow this link: " + "http://mapof.it/" + origin + '/' + destination, 'cyan')
+  if not options.nourl :
+       cprint ("To view these directions online, follow this link: " + "http://mapof.it/" + origin + '/' + destination, 'cyan')
 
   base_url = 'http://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&'
-  
+
   url = (base_url + url_end)[:-1]
-  print_path(url)
+  val =print_path(url,printInfo)
+  if val > 0:
+      return val
+  else:
+      return time
 
+def increment_time(options,valueNew, inc=5):
+  for key,value in options.__dict__.items():
+    if(value != None):
+      if key in ["departure_time", "arrival_time"]:
+        value = int(mktime(cal.parse(value)[0]))
+        # do increment
+        value = valueNew + 60 ;
+        options.__dict__[key] = str( value)
+        return options
 
-def print_path(url):
-  resp = requests.get(url)
-  respjson= simplejson.loads(resp.text)
-  checkresp(respjson, resp)
+def print_path(url, printInfo=True):
+
+  req = urllib2.Request(url)
+  response = urllib2.urlopen(req)
+  resp_text = response.read()
+  respjson= simplejson.loads(resp_text)
+
+  if respjson['status'] == 'OVER_QUERY_LIMIT' :
+        print "QUERY_OVERLIMIT"
+        time.sleep(1)
+        return -1;
+
+  checkresp(respjson, response)
 
   keypoints = respjson['routes'][0]['legs'][0]
 
-  print "From: " + keypoints['start_address']
-  print "To: " + keypoints['end_address']
-  print "Distance: " + keypoints['distance']['text']
-  print "Duration: " + keypoints['duration']['text'] 
+  if printInfo:
+    print "From: " + keypoints['start_address']
+    print "To: " + keypoints['end_address']
+    print "Distance: " + keypoints['distance']['text']
+    print "Duration: " + keypoints['duration']['text']
 
   printwarnings(respjson)
-  
+
   if 'mode=transit' in url:
-    print keypoints['departure_time']['text'] + ' to ' + keypoints['arrival_time']['text']
+    cprint( keypoints['departure_time']['text'], 'blue', end='')
+    sys.stdout.write("\t- ")
+    cprint( keypoints['arrival_time']['text'], 'blue')
 
   steps, linenum = keypoints['steps'], 1
   for step in steps:
     instruction = sanitize(step['html_instructions'])
     #fix for formatting issue on last line of instructions
     instruction = re.sub('Destination', '. Destination', instruction)
-    sys.stdout.write(str(linenum) + '. ' + instruction + ': ') 
+    sys.stdout.write(str(linenum) + '. ' + instruction + ': ')
     cprint(step['duration']['text'], 'green')
     linenum += 1
 
+  return keypoints['departure_time']['value']
 
 def sanitize(sentence):
   result = re.sub('<.*?>', '', sentence.encode('ascii', 'ignore'))
@@ -91,7 +146,7 @@ def checkinput(options):
 
 
 def checkresp(respjson, resp):
-  if resp.status_code != 200: 
+  if resp.code != 200:
     print "Sorry, something went wrong. Here is the output:"
     print resp.text
     sys.exit()
@@ -104,7 +159,7 @@ def checkresp(respjson, resp):
   except KeyError:
     print "No 'routes' in response"
 
-  try: 
+  try:
     respjson['routes'][0]
   except IndexError:
     print "Index out of range"
